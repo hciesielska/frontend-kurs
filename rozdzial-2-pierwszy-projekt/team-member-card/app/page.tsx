caef87f9c8a9a385b284fe660a1b3ef295fb831a
Bobrbezfutra <ciesielskahanna839@gmail.com>
2026-08-16 23:37:37 +0200

    feat: first Next.js project

---
+++ rozdzial-2-pierwszy-projekt/team-member-card/app/page.tsx
@@ -0,0 +1,27 @@
+import TeamMemberCard from './TeamMemberCard';
+
+export default function Page() {
+  const teamMembers = [
+    {
+      name: 'Hanna Ciesielska',
+      role: 'Studentka Politechniki Wrocławskiej',
+      bio: 'Jestem studentką informatyki. Biorę udział w wakacyjnym wyzwaniu Solvro.',
+      skills: ['TypeScript', 'React', 'Node.js']
+    }
+  ];
+
+  return (
+    <main className="flex min-h-screen items-center justify-center p-6 bg-rose-50/50">
+      {teamMembers.map((member, index) => (
+        <TeamMemberCard 
+          key={index}
+          name={member.name}
+          role={member.role}
+          bio={member.bio}
+          
+          skills={member.skills}
+        />
+      ))}
+    </main>
+  );
+}
